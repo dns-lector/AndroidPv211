74dc33c5d0e0f0f63d39bfe7739c11de1881cf60
dns-lector <157026463+dns-lector@users.noreply.github.com>
2025-03-26 20:49:31 +0200

    Initial commit

---
+++ app/src/main/java/itstep/learning/androidpv211/CalcActivity.java
@@ -0,0 +1,29 @@
+package itstep.learning.androidpv211;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class CalcActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_calc);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
+/*
+Д.З. Реалізувати верстку калькулятора:
+ - підібрати необхідні символи для операцій
+ - створити окремий стиль для кнопки "="
+ */
---
+++ app/src/main/java/itstep/learning/androidpv211/MainActivity.java
@@ -0,0 +1,38 @@
+package itstep.learning.androidpv211;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        findViewById( R.id.main_btn_calc ).setOnClickListener( this::onCalcButtonClick );
+
+    }
+
+    private void onCalcButtonClick( View view ) {
+        startActivity( new Intent( this, CalcActivity.class ) );
+    }
+}
+/*
+Д.З. Створити проєкт. Налаштувати конфігурацію / оточення запуску.
+Реалізувати розподіл ресурсів (літерали, кольори).
+Прикласти скріншоти запущеного проєкту.
+ */
